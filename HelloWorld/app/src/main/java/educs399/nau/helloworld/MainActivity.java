02fc59cd7c83beebeac574f99cef5aa9abc5dc6c
ChandlerAHayes <ChandlerAHayes@gmail.com>
2017-09-12 17:45:26 -0700

    HW3 with Hello World text on phone

---
+++ HelloWorld/app/src/main/java/educs399/nau/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package educs399.nau.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
